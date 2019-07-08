ce9fd5e511ce732842c6316ee89b9a23feb37037
PascalUlor <pascalulor@yahoo.com>
2019-07-08 13:19:19 +0100

    set up get all users api and set up default route to catch all wrong routes

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 // implement your API here
 const express = require("express");
-const lamb = require("./data/db");
+const Users = require("./data/db");
 
 const port = 3000 || process.env;
 const server = express();
 
 server.use(express.json());
+
+server.get("/api/users/", (req, res) => {
+  Users.find()
+    .then(users => {
+      console.log(res);
+      if (!users) {
+        res.status(404).json({
+          status: 404,
+          message: "No users available"
+        });
+      }
+      res.status(200).json(users);
+    })
+    .catch(err =>
+      res.status(500).json({
+        status: 500,
+        message: "internal server error"
+      })
+    );
+});
+
+/**
+ * All wrong routes
+ */
+server.get("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "Wrong route"
+  });
+});
 
 server.listen(port, () => {
   console.log(`server listening on port ${port}`);
