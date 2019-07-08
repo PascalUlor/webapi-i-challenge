4fa9c12a195e8a62715678f8c34cec828e02c3fe
PascalUlor <pascalulor@yahoo.com>
2019-07-08 12:40:18 +0100

    set up and run express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require("express");
+const lamb = require("./data/db");
+
+const port = 3000 || process.env;
+const server = express();
+
+server.use(express.json());
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
